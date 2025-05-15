800e3a04c2cfa4debace7106c5a70565f7c7c1d3
Miguel Angel <miguel.carrillo.m@hotmail.com>
2025-05-15 18:09:24 -0500

    Update grafana_app.py

--- grafana_app.py
+++ grafana_app.py
@@ -1,16 +1,13 @@
 import streamlit as st
-import streamlit.components.v1 as components
 
 st.title("Dashboard IoT del Clima")
 
-# Inserta el iframe dentro del componente HTML
-iframe_code = """
-<iframe 
-    src="https://miguelcmo.grafana.net/d-solo/aehqn58kr54aof/home-iot-weather-conditions?orgId=1&from=1747328396681&to=1747349996681&timezone=browser&refresh=10s&panelId=3&__feature.dashboardSceneSolo"
-    width="450" 
-    height="200" 
-    frameborder="0">
-</iframe>
-"""
+st.markdown(
+    """
+    ### Ver panel de clima
+    Debido a políticas de seguridad, el panel no puede mostrarse aquí directamente.
+    [Haz clic aquí para abrir el panel en una nueva pestaña.](https://miguelcmo.grafana.net/d-solo/aehqn58kr54aof/home-iot-weather-conditions?orgId=1&from=1747328396681&to=1747349996681&timezone=browser&refresh=10s&panelId=3&__feature.dashboardSceneSolo)
+    """,
+    unsafe_allow_html=True
+)
 
-components.html(iframe_code, height=220)  # deja un poco más de espacio vertical
